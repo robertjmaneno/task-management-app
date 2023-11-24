bcd3770cafdbe9d5a3db3c78b55657f04fb3c3cb
Robert J Maneno <robertjmaneno@gmail.com>
2023-11-24 15:24:24 +0200

    i have added some modifications to the file

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        title: const Text('Hello World'),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('Chart'),
+            Text('Expense List'),
+          ],
+        ),
+      ),
+    );
+  }
+}
